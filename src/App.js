d290b9ea8646c031370ca405964cc9fe82ae8cab
soyMoka <d15.cejas.matias@gmail.com>
2023-01-21 14:10:16 -0300

    add/remove tasks and task checkbox funcionalities done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
+import { nanoid } from 'nanoid';
+import React, { useState } from 'react';
 import Form from './components/Form/Form';
 import Todo from './components/Todo/Todo'
 import FilterButton from './components/FilterButton/FilterButton';
 
 function App(props) {
+
   
-  const taskList = props.tasks?.map((task) => (
-    <Todo 
-      id={task.id}
-      name={task.name} 
-      completed={task.completed} 
-      key={task.id + task.name}
-    />
+  const [tasks, setTasks] = useState(props.tasks)
+
+  const addTask = (name)=>{
+    let newTask = { id: `todo-${nanoid()}`, name, completed: false}
+    setTasks([...tasks, newTask])
+ 
+  }
+
+  const toggleTaskCompleted = (id) =>{
+    const updatedTasks = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose `completed` prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  const deleteTask = (id) =>{
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+  
+  const taskList = tasks?.map((task) => (
+      <Todo 
+        id={task.id}
+        name={task.name} 
+        completed={task.completed} 
+        key={task.id + task.name}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+      />
     )
   );
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+    
+      
+  
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
+      <h1>Simple To do</h1>
       
-      <Form />
+      <Form addTask={addTask}/>
 
       <div className="filters btn-group stack-exception">
         <FilterButton 
@@ -30,9 +67,11 @@ function App(props) {
           name='Completed'
         />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+
+      <h2 id="list-heading">{headingText}</h2>
+
       <ul
-        role="list"
+        //role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
         
