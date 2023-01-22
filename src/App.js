c8813a69470813f763ad7d3281f71205e8c94ac9
soyMoka <d15.cejas.matias@gmail.com>
2023-01-22 09:38:00 -0300

    readme eddited, favicon.ico eddited, edditing and filtering tasks

--- src/App.js
+++ src/App.js
@@ -4,17 +4,24 @@ import Form from './components/Form/Form';
 import Todo from './components/Todo/Todo'
 import FilterButton from './components/FilterButton/FilterButton';
 
+const FILTER_MAP = {
+  All:() => true,
+  Active:(task) => !task.completed,
+  Completed: (task) => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
-
   
-  const [tasks, setTasks] = useState(props.tasks)
-
+  const [filter, setFilter] = useState('All');
+  const [tasks, setTasks] = useState(props.tasks);
+  
   const addTask = (name)=>{
     let newTask = { id: `todo-${nanoid()}`, name, completed: false}
     setTasks([...tasks, newTask])
- 
+    
   }
-
+  
   const toggleTaskCompleted = (id) =>{
     const updatedTasks = tasks.map((task) => {
       // if this task has the same ID as the edited task
@@ -27,13 +34,25 @@ function App(props) {
     });
     setTasks(updatedTasks);
   }
-
+  
   const deleteTask = (id) =>{
     const remainingTasks = tasks.filter((task) => id !== task.id);
     setTasks(remainingTasks);
   }
   
-  const taskList = tasks?.map((task) => (
+  const editTask = (id, newName) => {
+    const editedTaskList = tasks.map((task)=>{
+      if (id === task.id){
+        return {...task, name: newName}
+      }
+      return task;
+    })
+    setTasks(editedTaskList);
+  }
+  
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
       <Todo 
         id={task.id}
         name={task.name} 
@@ -41,13 +60,23 @@ function App(props) {
         key={task.id + task.name}
         toggleTaskCompleted={toggleTaskCompleted}
         deleteTask={deleteTask}
+        editTask = {editTask}
       />
     )
   );
-
+  
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton 
+      key={name} 
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+        
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
-    
+
       
   
   return (
@@ -57,15 +86,9 @@ function App(props) {
       <Form addTask={addTask}/>
 
       <div className="filters btn-group stack-exception">
-        <FilterButton 
-          name='All'
-        />
-        <FilterButton 
-          name='Active'
-        />
-        <FilterButton 
-          name='Completed'
-        />
+        
+        {filterList}
+
       </div>
 
       <h2 id="list-heading">{headingText}</h2>
